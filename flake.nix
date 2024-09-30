656c05bced64d36953f6641caa31fb064bb47eab
David Strawn <davstrawn@jackhenry.com>
2024-09-30 16:19:43 -0500

    Flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Spanner CLI Nix Package";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {flake-utils, nixpkgs, ...}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        spanner-cli = pkgs.callPackage ./spanner-cli.nix {};
+      in
+        {
+          packages.default = spanner-cli;
+        }
+    );
+}
