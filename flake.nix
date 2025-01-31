db64b7a5d9bf4ea5f4ec50233508d937d6ffa746
Darren Gibson <zarthross+github@gmail.com>
2025-01-31 09:32:57 -0600

    format

--- flake.nix
+++ flake.nix
@@ -5,24 +5,27 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
-  outputs = inputs@{flake-parts, nixpkgs, ...}:
-  flake-parts.lib.mkFlake {inherit inputs;} {
-    imports = [
-      inputs.flake-parts.flakeModules.easyOverlay
-      inputs.treefmt-nix.flakeModule
-    ];
-    systems = [
-      "x86_64-darwin"
-      "aarch64-darwin"
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    perSystem = {config, pkgs, ...}: {
-      packages.spanner-cli = pkgs.callPackage ./spanner-cli.nix {};
-      treefmt = {
-        projectRootFile = "flake.nix";
-        programs.nixfmt.enable = true;
-      };
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+        inputs.treefmt-nix.flakeModule
+      ];
+      systems = [
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          packages.spanner-cli = pkgs.callPackage ./spanner-cli.nix { };
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+          };
+        };
     };
-  };
 }
--- spanner-cli.nix
+++ spanner-cli.nix
@@ -1,4 +1,9 @@
-{ stdenv, pkgs, lib, fetchFromGitHub }:
+{
+  stdenv,
+  pkgs,
+  lib,
+  fetchFromGitHub,
+}:
 
 let
   version = "0.10.5";
@@ -13,7 +18,5 @@ pkgs.buildGoModule {
     sha256 = "sha256-4umtZFsEYu0JI4l3PT4A3KPpemjVPwQMweU5j/dcnNs=";
   };
   vendorHash = "sha256-m6/M9gD+iRAmS4tQ+tHoqWzI8mZTN1RUuqCYaPTsSK8=";
-  patches = [
-    ./spanner-cli-0.10.5-tidy.patch
-  ];
+  patches = [ ./spanner-cli-0.10.5-tidy.patch ];
 }
